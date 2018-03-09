d450dd8b350b39fb5770c3701fc46dd5886f693c
dwarsz <36249083+dwarsz@users.noreply.github.com>
2018-03-08 22:45:00 -0500

    Update DeckTest.java

--- FlamingoBLL/src/test/java/pkgCore/DeckTest.java
+++ FlamingoBLL/src/test/java/pkgCore/DeckTest.java
@@ -11,6 +11,11 @@ public class DeckTest {
 		
 		//	TODO: Build a deck(1), make sure there are 52 cards in the deck
 		//	TODO: Build a deck(6), make sure there are 312 cards in the deck 
+		Deck d1 = new Deck(1);
+		Deck d2 = new Deck(6);
+		System.out.print(d1.cards.size());
+		System.out.print(d2.cards.size());
+		
 	}
 
 }
